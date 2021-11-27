03d54ce56a618db19a2e39667aa72b65502302a9
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-27 11:10:33 +0300

    Литеральные типы

--- test.ts
+++ test.ts
@@ -1,53 +1,53 @@
-type coord = {lat: number, long: number}
+const a = 'sdfgee' // константа имеет свое значение как тип
 
-interface ICoord {
-    lat: number
-    long: number
+let b: 'hi' = "hi" //объявляем такой тип (правда так делать бесполезно)
+
+type direction = 'left' | 'right';
+
+function moveDog(direction: direction): -1 | 0 | 1 {
+    switch (direction) {
+        case 'left':
+            return -1;
+        case 'right':
+            return 1
+        default: // нужно обязательно обрабатывать, потому что в рантайме TS нет
+            return 0
+    }
 }
 
-type ID = number | string; //с типами так можно, с интерфейсом нет
+moveDog('left') // теперь мы можем сюда передавать толко left и right
 
+interface IConnection {
+    host: string
+    port: number
+}
 
-function compute (coord: {lat: number, long: number}) {
+function connect(connection: IConnection | "default") {
+    // можно комбинировать строковые литераты с обычными типами и интерфейсами
 
 }
 
-interface IAnimal {
-    name: string
+connect('default')
+
+const connection = {
+    host: 'localhost',
+    protocol: 'https' // имеет тип Sting
 }
 
-interface IDog extends IAnimal {
-    tail?: boolean // добавляем к Animal свойство
+function connect2(host: string, protocol: 'http' | 'https') {
+
 }
 
-const dog: IDog = {
-    name: 'sdf'
+// connect2(connection.host, connection.protocol)
+// не будет работать, т.к. protocol имеет строчный тип и может быть изменен
+
+const connection2 = {
+    host: 'localhost',
+    protocol: 'https' as 'https' // объяляем тип 'https'
 }
 
-type Animal = {
-    name: string,
-}
+connect2(connection.host, connection2.protocol)
 
-type Dog = Animal & { // наследование через types, но интерфейсы удобнее
-    tail: boolean
-}
-
-interface IDog2 {
-    name: string
-}
-
-interface IDog2 {
-    tail: boolean 
-}
-
-const dog2: IDog2 = { // в таких случаях интерфейсы объединяются между собой
-    // с type такое работать не будет
-    name: 'dsg',
-    tail: true
-}
-
-//types не могут участвовать в слиянии определений
-//interfaces могут определять только объеты, а не примитивы
-
-// всегда использовать intarface, если не нужна какая-то фича types
-
+const c: any = 5
+let d: number = c as number // преобразовать any в number
+let e = <number>c // другой способ (но лучше его не использовать, он не везде работает)
