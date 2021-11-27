12a6c8200cc8a3ff00f7dfea14acaad5ad6180f2
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-27 09:29:38 +0300

    Промежуточные обработчики ошибок

--- users/users.js
+++ users/users.js
@@ -2,6 +2,16 @@ import express from 'express'
 
 const userRouter = express.Router(); // Router к которому мы можем привязывать все обработчики запросов
 
+userRouter.use((req, res, next) => { // обработчик для обработчика
+    console.log('Обработчик users')
+    next()
+})
+
+userRouter.use('/hello', (req, res, next) => { // обработчик для всех запросов hello обработчика user Router
+    console.log('Обработчик users')
+    next()
+})
+
 userRouter.post('/login', (req, res) => {
     res.send('login')
 })
