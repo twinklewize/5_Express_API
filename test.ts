275b88a7c05fa66adb0db8632ee111db6891cded
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-27 10:38:56 +0300

    Union типы

--- test.ts
+++ test.ts
@@ -1,29 +1,18 @@
-let a: number = 5
-let b: string = 'sfs'
+let universalId: number | string = 5 // union тип
+universalId = 'sdf'
 
-let c: number = a + Number(b)
-
-let d: boolean = true
-
-let names: string[] = ['sd', 'sdfsd']
-
-let tup: [number, string] = [2, 'sdfds'] // кортеж
-tup.push('adfas') // можно добавить 3 элемент, но к нему нельзя обратиться
-
-let e: any = 5; // any совместима с javascript
-e = 'dsf'
-e = true
-
-let anyArr: any[] = ['sdf', 3, true]
-
-function greet(name: string) {
-    return name + 'Hi'
+function printId(id: number | string) {
+    if (typeof id == 'string') {
+        console.log(id.toUpperCase())
+    } else {
+        id++;
+    }
 }
 
-names.map(x => x)
-names.map((x: string) => x) // работа с анонимными функциями
-
-
-function coord(coord: { lat: number, long?: number }) { // объявляем объектный тип в аргументе функции
-
+function helloUser(user: string | string[]) {
+    if (Array.isArray(user)) {
+        console.log(user.join(', ') + 'Hi!')
+    } else {
+        console.log(user + 'Hi!')
+    }
 }
