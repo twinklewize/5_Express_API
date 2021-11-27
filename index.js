c4c7b42b67ba20b3fe765aacdf35752a23b6faf8
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-27 08:59:48 +0300

    Ответы клиенту

--- index.js
+++ index.js
@@ -4,43 +4,35 @@ import express from 'express'
 const port = 8000
 const app = express() // создаем приложение
 
+app.get('/hello', (req, res) => {
+    // res.send({ success: true }) // отправка json
+    // res.status(201).send({ success: true }) // статус код
+    // res.status(201).json({ success: true }) // отправка толкьо json
+    // res.download('/file_path.pdf', 'New_File_Name.pdf') // передать ссылку на скачиваине
+    // res.redirect(301, 'https://example.com') // перенаправляем пользователя на другой сайт
+    // res.set('Content-Type', 'text/plain') // вызываем перед методом send, задант тип контента
+    // res.append('Warning', 'code') // вызываем перед методом send, добавляет новый кастомный заголовок
+    // res.type('application/json') // задаем Content-Type
 
-//all должен быть вызван в начале
-app.all('/hello', (req, res, next) => {
-    //hel?lo: hello, helo; 
-    //hel+lo: hello, hellllo; 
-    //hel*lo: helqlo, hello, helerer3r3lo
-    //he(la)?lo: helo, helalo
-    //regex тоже принимается: /.*a$/ - строка оканчивающаяся на a (вместо '/hello')
-    // значит что при любом запросе будет дополнително обработана эта функцию
-    console.log('All') // каждый раз выводится в консоль, когда клиент получает ответ от сервера
-    next() // next - функция перейдет следующему обработчику
+    // Еще можно:
+    // res.location('')
+    // res.links({
+    //     next: 'sdfsfd'
+    // })
+    // res.cookie('token', 'sam_token', {
+    //     domain: '',
+    //     path: '/',
+    //     secure: true,
+    //     expires: 60000
+    // })
+    // res.clearCookie('token')
+    // res.clearCookie('token', { path })
+
+    res.status(404).end() // вернуть not_found
+    res.end()
+
+    // res.send('Привет!')
 })
-
-const cb = (req, res, next) => { // callback (здесь попадаем сначала в all потом в callback, потом в get)
-    console.log('CB')
-    next()
-}
-
-app.route('/user')
-    .get('/hello', (req, res) => {
-        res.send('Привет GET!')
-    })
-    .post('/hello', (req, res) => {
-        res.send('Привет Post!')
-    })
-
-
-// get with callback
-// app.get('/hello', cb, (req, res) => { 
-//     res.send('Привет мой друг!')
-// })
-
-
-// вариант с массивом из callback'ов
-// app.get('/hello', [cb, cb, (req, res) => { 
-//     res.send('Привет мой друг!')
-// }])
 
 
 app.listen(port, () => { // нужно чтобы слушать запросы
