ccc1f9611e86d3a20d92be0edfa31ac7622f2156
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-28 10:31:03 +0300

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { App } from "./app"
+import { LoggerService } from "./logger/logger.service"
 
 async function bootstrap() {
-    const app = new App()
+    const app = new App(new LoggerService()) // простейший Dependency Injection
+    // проблема такого DI в том, что придется пробрасывать на несколько уровней один Logger
     await app.init()
 }
 
