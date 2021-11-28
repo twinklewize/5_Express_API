a7f047bb65cf6984c6e406c7cc748ae1123b308c
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-28 11:29:25 +0300

    Users-Controller

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,25 @@ import express, { Express } from 'express'
 import { userRouter } from './users/users'
 import { Server } from 'http'
 import { LoggerService } from './logger/logger.service'
+import { UserController } from './users/users.conroller'
 
 export class App {
     app: Express
     server: Server
     port: number
     logger: LoggerService
+    userConroller: UserController
 
-    constructor(logger: LoggerService) {
+    constructor(logger: LoggerService,
+        userConroller: UserController) {
         this.app = express()
         this.port = 8000
         this.logger = logger
+        this.userConroller = userConroller
     }
 
     useRoutes() {
-        this.app.use('/users', userRouter)
+        this.app.use('/users', this.userConroller.router)
     }
 
     public async init() {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { App } from "./app"
 import { LoggerService } from "./logger/logger.service"
+import { UserController } from "./users/users.conroller"
 
 async function bootstrap() {
-    const app = new App(new LoggerService()) // простейший Dependency Injection
+    const logger = new LoggerService
+    const app = new App(logger, new UserController(logger)) // простейший Dependency Injection
     // проблема такого DI в том, что придется пробрасывать на несколько уровней один Logger
     await app.init()
 }
