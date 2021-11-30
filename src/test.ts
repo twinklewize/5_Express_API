763c676fa44288bfb8b11cc72365d7d8fe795a16
twinklewize <ivan.stanovoy1337@gmail.com>
2021-11-30 10:52:47 +0300

    Metadata Reflection Test

--- src/test.ts
+++ src/test.ts
@@ -1,90 +1,18 @@
-// function Component(target: Function) {
-//     console.log(target)
-// }
+import 'reflect-metadata'
 
-
-
-// @Component
-// export class User {
-//     id: number
-
-//     updateId(newId: number) {
-//         this.id = newId
-//         return this.id
-//     }
-// }
-
-// console.log(new User().id)
-
-
-function Logger() {
-    console.log('init Logger')
-    return (target: Function) => {
-        console.log('run Logger')
-    }
+function Test(target: Function) {
+    Reflect.defineMetadata('a', 1, target)
+    const meta = Reflect.getMetadata('a', target)
+    console.log(meta)
 }
 
+function Prop(target: Object, name: string){
 
-function Component2(id: number) {
-    console.log('init component')
-    return (target: Function) => {
-        console.log('run component')
-        target.prototype.id = id
-    }
 }
 
-function Method(
-    target: Object,
-    propertyKey: string,
-    propertyDescriptor: PropertyDescriptor
-) {
-    console.log('Метод: ' + propertyKey)
-    const oldValue = propertyDescriptor.value
-    propertyDescriptor.value = function (...args: any[]) {
-        // oldValue()
-        return args[0] * 10
-    }
+@Test
+class C {
+    @Prop prop: number
 }
 
-function Prop(
-    target: Object,
-    propertyKey: string) {
-    let value: number
-    const getter = () => {
-        console.log('Get!')
-        return value
-    }
-    const setter = (newValue: number) => {
-        console.log('Set!')
-
-        value = newValue
-    }
-
-    Object.defineProperty(target, propertyKey, {
-        get: getter,
-        set: setter
-    })
-}
-
-function Param(
-    target: Object,
-    propertyKey: string,
-    index: number) {
-        console.log(propertyKey, index)
-}
-
-// инициализация в таком порядке, отработка в обратном порядке
-@Logger()
-@Component2(1)
-export class User2 {
-    @Prop id: number
-
-    @Method
-    updateId(@Param newId: number) {
-        this.id = newId
-        return this.id
-    }
-}
-
-console.log(new User2().id)
-console.log(new User2().updateId(2))
+// метадата помогает делать проверку типов в рантайме (описано в Prop)
